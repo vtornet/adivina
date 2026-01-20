5c72dd62c1552d16fce60b03dc17c584e7b71128
Vtornet <vtornet@gmail.com>
2026-01-20 22:06:03 +0100

    Fix server.js to CommonJS (remove ES module imports)

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+require('dotenv').config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
