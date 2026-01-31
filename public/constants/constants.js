8669248119718f5e205079a162933ea7eb9a9b76
Álvaro Zájara <alzca1@gmail.com>
2026-01-31 13:44:21 +0100

    refactor: add general app constants

---
+++ public/constants/constants.js
@@ -0,0 +1,7 @@
+export const APP_VERSION = "v1.3.1 (Fixed)";
+export const CANONICAL_PROD_ORIGIN = "https://adivinalacancion.app";
+export const NOTIFICATIONS_STORAGE_KEY = "localNotifications";
+export const NOTIFICATIONS_PROMPTED_KEY = "inviteNotificationsPrompted";
+export const PERMISSIONS_STORAGE_KEY = "userPermissions";
+export const FINISHED_NOTIFICATIONS_KEY = "finishedOnlineNotifications";
+export const ADMIN_EMAIL = "vtornet@gmail.com";
